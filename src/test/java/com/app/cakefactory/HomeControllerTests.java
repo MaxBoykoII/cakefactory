32e05e04fce23bf481f8d7564d28a53e85dec012
Max Boyko <maxboykomax@gmail.com>
2020-08-25 11:38:57 -0700

    test: Add unit test for the home controller

---
+++ src/test/java/com/app/cakefactory/HomeControllerTests.java
@@ -0,0 +1,33 @@
+package com.app.cakefactory;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+
+import com.app.cakefactory.controller.HomeController;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("should return the landing page")
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Welcome to the Cake Factory!")
+                ));
+    }
+}
